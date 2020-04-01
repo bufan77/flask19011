c0a7272094329da8bb70ebbe92de74f2d1d4b9b6
bufan <1173324376@qq.com>
2020-04-01 16:40:58 +0800

    dongtaiurl

--- day01/watchlist/app.py
+++ day01/watchlist/app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello, Flask</h1>"
+    return "<h1>Hello, Flask 中国<h1>"
+@app.route('/index/<name>')
+def home(name):
+    print(url_for("home",name="Bruce"))
+    return "<h1>Hello, %s</h1>"%name
